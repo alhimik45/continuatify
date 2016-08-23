11b1144cbb7d0683a9c6b21b58e08eee4ad83dd9
alhimik45 <alhimik45@gmail.com>
2016-08-23 11:13:08 +0700

    add source map support

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var continuation = require('continuation');
 var through = require('through2');
+var sourceMap = require('source-map');
+var SourceMapGenerator = sourceMap.SourceMapGenerator;
+var SourceMapConsumer = sourceMap.SourceMapConsumer;
+var convertSourceMap = require('convert-source-map');
 
 
-module.exports = function continuatify(file, options) {
+module.exports = function continuatify(filename, options) {
     var chunks = [];
 
     function ondata(buffer, encoding, cb) {
@@ -14,7 +18,32 @@ module.exports = function continuatify(file, options) {
 
         try {
             var code = chunks.join('');
+
+            var mapBefore = null;
+            if (options._flags.debug) {
+                mapBefore = convertSourceMap.fromSource(code);
+                if (mapBefore) {
+                    mapBefore = mapBefore.toObject();
+                }
+                options.sourceMap = true;
+            }
+
             var compiled = continuation.compile(code, options);
+
+            if (options._flags.debug) {
+                var file = filename;
+                if (mapBefore) {
+                    file = mapBefore.file;
+                }
+                var continuationMap = continuation.getSourceMap(file, [file]);
+                var resultMap = convertSourceMap.fromJSON(continuationMap);
+                if (mapBefore) {
+                    var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(JSON.parse(continuationMap)));
+                    generator.applySourceMap(new SourceMapConsumer(mapBefore));
+                    resultMap = convertSourceMap.fromJSON(generator.toString());
+                }
+                compiled += "\n" + resultMap.toComment();
+            }
             this.push(compiled);
             cb();
         } catch (e) {
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 /* global describe, it */
 
 var assert = require('assert');
-var Readable = require('stream').Readable
+var Readable = require('stream').Readable;
 var continuatify = require('./index');
 var continuation = require('continuation');
+var convertSourceMap = require('convert-source-map');
 
 var code = `function fibonacci() {
                 var a = 0, current = 1;
@@ -22,9 +23,27 @@ describe('continuatify', function () {
         s.push(code);
         s.push(null);
 
-        s.pipe(continuatify())
+        s.pipe(continuatify('test.js', {_flags: {}}))
             .once('data', function (buffer) {
                 assert.equal(buffer.toString('utf8'), continuation.compile(code));
+                done();
+            });
+    });
+
+    it('should emit same source map as plain continuation', function (done) {
+        var s = new Readable;
+        s.push(code);
+        s.push(null);
+
+        var plainCompiled = continuation.compile(code, {sourceMap: true});
+        var plainSourceMap = convertSourceMap
+            .fromJSON(continuation.getSourceMap('test.js', ['test.js']))
+            .toComment();
+        var plainCode = plainCompiled + "\n" + plainSourceMap;
+
+        s.pipe(continuatify('test.js', {_flags: {debug: true}}))
+            .once('data', function (buffer) {
+                assert.equal(buffer.toString('utf8'), plainCode);
                 done();
             });
     });
